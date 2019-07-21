b26b37da6e74cd404d8eb1b504f51503374d3caf
iancatface <b7official@gmail.com>
2019-07-21 20:14:44 +0300

    initial commit. added app tweets, made dirs

---
+++ twitter_clone/tweets/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# class Tweet(models.Model):
+#     author = models.ForeignKey(User, related_name='tweets', on_delete=models.CASCADE)

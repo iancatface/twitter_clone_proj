44da8ef451b94117d4ece06fdd33e3ec35be0230
iancatface <b7official@gmail.com>
2019-07-22 13:52:49 +0300

    wrote my custon login and logout

--- twitter_clone/accounts/views.py
+++ twitter_clone/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from tweets.models import Tweet
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        # message = 'ur already logined'
+        return redirect('tweets:tweet_list')
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_user = authenticate(request, username=cd['username'], password=cd['password'])
+            if new_user is not None:
+                if new_user.is_active:
+                    login(request, new_user)
+                    return HttpResponse('eyy ur logined')
+                else:
+                    return HttpResponse('ur not active')
+            else:
+                return HttpResponse('username and password didn\'t match')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
